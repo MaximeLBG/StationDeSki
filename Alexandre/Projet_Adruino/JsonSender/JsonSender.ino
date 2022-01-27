baa14bc8e13fcf6da44c9930568e93bce0c8eb25
AlexandrePILOT <alexandrepilot.theone@gmail.com>
2022-01-27 11:44:57 +0100

    ajout du programme JsonSender

---
+++ Alexandre/Projet_Adruino/JsonSender/JsonSender.ino
@@ -0,0 +1,58 @@
+#include <LoRa.h>
+#include "boards.h"
+#include <ArduinoJson.h>
+
+char output[128];
+int counter = 0;
+
+void setup() {
+  
+    initBoard();
+    // When the power is turned on, a delay is required.
+    delay(1500);
+
+    Serial.println("LedSender");
+    LoRa.setPins(RADIO_CS_PIN, RADIO_RST_PIN, RADIO_DI0_PIN);
+    if (!LoRa.begin(LoRa_frequency)) {
+        Serial.println("Le lora n'a pas démmarer");
+        while (1);
+    }
+  
+  StaticJsonDocument<128> doc;
+  
+  JsonObject Capteur = doc.createNestedObject("Capteur");
+  Capteur["numero"] = "1";
+  Capteur["vitesseVent"] = "10";
+  Capteur["directionVent"] = "N";
+  Capteur["temperature"] = "20";
+  
+  serializeJson(doc, output);
+}
+
+void loop(){
+  
+  int i; //var de la boucle for
+    char buf[256]; //buffer pour faire apparaitre le texte
+
+    while(1)
+    {
+      for(i=0;i<=1;i++)//boucle pour faire clignoter la led
+      {
+        LoRa.beginPacket(); //début de l'envoie des packets
+        LoRa.print(output); //envoie du packet
+        Serial.println(output); //affichage dans le moniteur série
+        LoRa.endPacket(); //fin de l'envoie des packets
+        delay(1000);
+        counter++;
+        Serial.println(counter);//affichage dans le moniteur série du nombre d'itération
+
+        u8g2->clearBuffer(); // effacement de tout l'affichage
+        u8g2->drawStr(0, 12, "Transmission: OK!"); // affichage d'une string a l'écran
+        snprintf(buf, sizeof(buf), "Envoie numero: %d", counter); //permet de formater les valeurs a l'affichage
+        u8g2->drawStr(0, 26, buf);
+        snprintf(buf, sizeof(buf), "Message: %d", output);
+        u8g2->drawStr(0, 40, buf);
+        u8g2->sendBuffer(); //permet d'envoyer la compositon a l'écran
+      }
+    }
+}
