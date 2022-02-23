b43ab740dce44eeb69e9f1c3b6dc07bd05a0c7e9
AlexandrePILOT <alexandrepilot.theone@gmail.com>
2022-02-23 10:03:15 +0100

    final json sender/reciver

--- Alexandre/Projet_Adruino/JsonReceiver/JsonReceiver.ino
+++ Alexandre/Projet_Adruino/JsonReceiver/JsonReceiver.ino
@@ -2,16 +2,15 @@
 #include "boards.h"
 #include <ArduinoJson.h>
 
-char* input;
 size_t inputLength;
 
-void setup()
-{
+void setup(){
+  
     initBoard();
     // When the power is turned on, a delay is required.
     delay(1500);
 
-    Serial.println("LedReceiver");
+    Serial.println("JsonReceiver");
 
     LoRa.setPins(RADIO_CS_PIN, RADIO_RST_PIN, RADIO_DI0_PIN);
     if (!LoRa.begin(LoRa_frequency)) {
@@ -24,56 +23,45 @@ void loop()
 {
     // try to parse packet
     int packetSize = LoRa.parsePacket();
+     
     if (packetSize) {
         
-        String recv = "";
+        String recv ;
         // read packet
         while (LoRa.available()) {
           recv += (char)LoRa.read(); //permet de réceptionner les données reçues
         }
+          Serial.println(recv);
+
+        StaticJsonDocument<192> doc;
         
-          StaticJsonDocument<192> doc;
-          DeserializationError error = deserializeJson(doc, recv);
-          
-          if (error) {
-            Serial.print("deserializeJson() failed: ");
-            Serial.println(error.c_str());
-            return;
-          }
-          
-          JsonObject Capteur = doc["Capteur"];
-          const char* Capteur_numero = Capteur["numero"]; // "1"
-          const char* Capteur_vitesseVent = Capteur["vitesseVent"]; // "10"
-          const char* Capteur_directionVent = Capteur["directionVent"]; // "R"
-          const char* Capteur_temperature = Capteur["temperature"]; // "20"
+        DeserializationError error = deserializeJson(doc, recv);
 
-          Serial.print("Capteur numéro : ");
-          Serial.println(Capteur_numero);
-
-          Serial.print("Vitesse du vent : ");
-          Serial.println(Capteur_vitesseVent);
-
-          Serial.print("Direction du vent : ");
-          Serial.println(Capteur_directionVent);
-          
-          Serial.print("Temperature : ");
-          Serial.println(Capteur_temperature);   
-
-          Serial.print("complet : ");
-          Serial.println(Capteur);
+        if (error) {
+          Serial.print("deserializeJson() failed: ");
+          Serial.println(error.c_str());
+          return;
+        }
+        
+        int capteur = doc["capteur"]; // "1"
+        float vitesseVent = doc["vitesseVent"]; // "10"
+        float directionVent = doc["directionVent"]; // "N"
+        float temperature = doc["temperature"]; // "20"  
         
         char buf[256]; //buffer pour faire apparaitre le texte
-        
+
         u8g2->clearBuffer(); // effacement de tout l'affichage
-        u8g2->drawStr(0, 12, "Transmission: OK!"); // affichage d'une string a l'écran
-        snprintf(buf, sizeof(buf), "Capteur: %s", Capteur_numero); //permet de formater les valeurs a l'affichage
+        snprintf(buf, sizeof(buf), "Capteur: %i", capteur); //permet de formater les valeurs a l'affichage
+        u8g2->drawStr(0, 12, buf);
+        snprintf(buf, sizeof(buf), "Vitesse: %.1f", vitesseVent);
         u8g2->drawStr(0, 26, buf);
-        snprintf(buf, sizeof(buf), "Vitesse: %s", Capteur_vitesseVent);
+        snprintf(buf, sizeof(buf), "Direction: %.f", directionVent);
         u8g2->drawStr(0, 40, buf);
-        snprintf(buf, sizeof(buf), "Direction: %s", Capteur_directionVent);
+        snprintf(buf, sizeof(buf), "Temperature: %.1f", temperature);
         u8g2->drawStr(0, 54, buf);
-        snprintf(buf, sizeof(buf), "Temperature: %s", Capteur_temperature);
+        u8g2->drawStr(0, 68, "Reciver");
         u8g2->sendBuffer(); //permet d'envoyer la compositon a l'écran
 
         }
+        
 }
--- Alexandre/Projet_Adruino/JsonSender/JsonSender.ino
+++ Alexandre/Projet_Adruino/JsonSender/JsonSender.ino
@@ -2,13 +2,8 @@
 #include "boards.h"
 #include <ArduinoJson.h>
 
-String output;
-int counter = 0;
 
-int numeroCapteur = 1;
-float vitesseVent = 10.1;
-float directionVent = 142.2;
-float temperature = 20.4;
+int counter = 0;
 
 void setup() {
   
@@ -16,43 +11,50 @@ void setup() {
     // When the power is turned on, a delay is required.
     delay(1500);
 
-    Serial.println("LedSender");
+    Serial.println("JsonSender");
     LoRa.setPins(RADIO_CS_PIN, RADIO_RST_PIN, RADIO_DI0_PIN);
     if (!LoRa.begin(LoRa_frequency)) {
         Serial.println("Le lora n'a pas démmarer");
         while (1);
     }
-  
-  StaticJsonDocument<128> doc;
-  
-  JsonObject Capteur = doc.createNestedObject("Capteur");
-  Capteur["numero"] = numeroCapteur;
-  Capteur["vitesseVent"] = vitesseVent;
-  Capteur["directionVent"] = directionVent;
-  Capteur["temperature"] = temperature;
-  
-  serializeJson(doc, output);
 }
 
 void loop(){
-  
+
+  String output="";
   int i; //var de la boucle for
   char buf[256]; //buffer pour faire apparaitre le texte
+  float temperature = 20.4;
+  float directionVent = 142.2;
+  float vitesseVent = 10.4;
+  float capteur = 1;
 
+        StaticJsonDocument<96> doc;
+    
+        doc["capteur"] = capteur;
+        doc["vitesseVent"] = vitesseVent;
+        doc["directionVent"] = directionVent;
+        doc["temperature"] = temperature;
+        
+        serializeJson(doc, output);
 
+        delay(1000);
         LoRa.beginPacket(); //début de l'envoie des packets
         LoRa.print(output); //envoie du packet
         Serial.println(output); //affichage dans le moniteur série
-        LoRa.endPacket(); //fin de l'envoie des0 packets
+        LoRa.endPacket(); //fin de l'envoie des packets
         delay(1000);
-        counter++;
-        Serial.println(counter);//affichage dans le moniteur série du nombre d'itération
-
-        u8g2->clearBuffer(); // effacement de tout l'affichage
-        u8g2->drawStr(0, 12, "Transmission: OK!"); // affichage d'une string a l'écran
-        snprintf(buf, sizeof(buf), "Envoie numero: %d", counter); //permet de formater les valeurs a l'affichage
+        
+        u8g2->clearBuffer(); // effacement de tout l'affichagen
+        snprintf(buf, sizeof(buf), "Capteur: %.f", capteur); //permet de formater les valeurs a l'affichage
+        u8g2->drawStr(0, 12, buf);
+        snprintf(buf, sizeof(buf), "Vitesse: %.1f", vitesseVent);
         u8g2->drawStr(0, 26, buf);
-        snprintf(buf, sizeof(buf), "Message: %d", output);
+        snprintf(buf, sizeof(buf), "Direction: %.f", directionVent);
         u8g2->drawStr(0, 40, buf);
+        snprintf(buf, sizeof(buf), "Temperature: %.1f", temperature);
+        u8g2->drawStr(0, 54, buf);
+        u8g2->drawStr(0, 68, "Sender");
         u8g2->sendBuffer(); //permet d'envoyer la compositon a l'écran
+        
 }
