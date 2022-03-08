7bfc374da68d5dffc8abd3838626d2183a6e6020
AlexandrePILOT <alexandrepilot.theone@gmail.com>
2022-03-08 16:10:11 +0100

    fonction

--- Alexandre/Projet_Adruino/JsonSender/JsonSender.ino
+++ Alexandre/Projet_Adruino/JsonSender/JsonSender.ino
@@ -2,7 +2,6 @@
 #include "boards.h"
 #include <ArduinoJson.h>
 
-
 int counter = 0;
 
 void setup() {
@@ -19,42 +18,51 @@ void setup() {
     }
 }
 
-void loop(){
-
-  String output="";
-  int i; //var de la boucle for
-  char buf[256]; //buffer pour faire apparaitre le texte
-  float temperature = 20.4;
-  float directionVent = 142.2;
-  float vitesseVent = 10.4;
-  float capteur = 1;
-
-        StaticJsonDocument<96> doc;
+void affichage(int idCapteur,float dirvent,float vitesse,float temp)
+{
+    char buf[256]; //buffer pour faire apparaitre le texte
     
-        doc["idCapteur"] = capteur;
-        doc["vitesse"] = vitesseVent;
-        doc["dirvent"] = directionVent;
-        doc["temp"] = temperature;
-        
-        serializeJson(doc, output);
-
-        delay(1000);
-        LoRa.beginPacket(); //début de l'envoie des packets
-        LoRa.print(output); //envoie du packet
-        Serial.println(output); //affichage dans le moniteur série
-        LoRa.endPacket(); //fin de l'envoie des packets
-        delay(1000);
-        
         u8g2->clearBuffer(); // effacement de tout l'affichagen
-        snprintf(buf, sizeof(buf), "Capteur: %.f", capteur); //permet de formater les valeurs a l'affichage
+        snprintf(buf, sizeof(buf), "Capteur: %i", idCapteur); //permet de formater les valeurs a l'affichage
         u8g2->drawStr(0, 12, buf);
-        snprintf(buf, sizeof(buf), "Vitesse: %.1f", vitesseVent);
+        snprintf(buf, sizeof(buf), "Vitesse: %.1f", vitesse);
         u8g2->drawStr(0, 26, buf);
-        snprintf(buf, sizeof(buf), "Direction: %.f", directionVent);
+        snprintf(buf, sizeof(buf), "Direction: %.f", dirvent);
         u8g2->drawStr(0, 40, buf);
-        snprintf(buf, sizeof(buf), "Temperature: %.1f", temperature);
+        snprintf(buf, sizeof(buf), "Temperature: %.1f", temp);
         u8g2->drawStr(0, 54, buf);
         u8g2->drawStr(0, 68, "Sender");
         u8g2->sendBuffer(); //permet d'envoyer la compositon a l'écran
+}
+
+void sendLoRa(String strJson)
+{
+        delay(1000);
+        LoRa.beginPacket(); //début de l'envoie des packets
+        LoRa.print(strJson); //envoie du packet
+        Serial.println(strJson); //affichage dans le moniteur série
+        LoRa.endPacket(); //fin de l'envoie des packets
+        delay(1000);
+}
+
+void loop(){
+
+        String output="";
+        int i; //var de la boucle for
+        float temp = 20.4;
+        float dirvent = 142.2;
+        float vitesse = 10.4;
+        float idCapteur = 1;
+
+        StaticJsonDocument<96> doc;
+    
+        doc["idCapteur"] = idCapteur;
+        doc["vitesse"] = vitesse;
+        doc["dirvent"] = dirvent;
+        doc["temp"] = temp;
         
+        serializeJson(doc, output);
+
+        sendLoRa(output);
+        affichage(idCapteur,dirvent,vitesse,temp);    
 }
