79816cf71447ae7af51b59d9369f5d7b57072a35
Maxime LEBEGUE <maxime.lebegue@cdfnancy.fr>
2022-04-07 09:03:45 +0200

    Prog EEPROM finit

--- Maxime/EEPROM/EEPROM.ino
+++ Maxime/EEPROM/EEPROM.ino
@@ -1,45 +1,37 @@
 #include <EEPROM.h>
 
-#define EEPROM_SIZE 3
+#define EEPROM_SIZE 10
 
 void setup() 
 {
  Serial.begin(115200);
  EEPROM.begin(EEPROM_SIZE);
 
- String strIdModule;
+ String strIdModule, yesno;
  strIdModule = EEPROM.read(0);
- char yesno;
  int result;
  
  Serial.println("L'ancien module est : " + strIdModule);
  Serial.println("Voulez-vous saisir un nouveau module ? (y/n)");
  
+ Serial.setTimeout(5000);
+ yesno = Serial.readStringUntil('\n');
  
- while (Serial.available()==0)
+ if(yesno == "n" || yesno == 0)
  {
-   Serial.setTimeout(5000);
-   yesno = Serial.read();
-   
+    Serial.println("Vous êtes sur le module : " + strIdModule);
  }
- 
- if(yesno != 'n')
+ else
  {
   Serial.println("Saisir le nouveau module : ");
-  strIdModule = Serial.readStringUntil('\n');
+  strIdModule = Serial.readString();
   
-  Serial.println("test2");
   result = strIdModule.toInt();  //converti un string en char
   
   EEPROM.write(0, result);  //deuxième valeur ne doit pas être un string 
   EEPROM.commit();
-  Serial.println(result);
+  Serial.print("Le nouveau module est : "); Serial.println(result);
  }
- else if(yesno == 'n')
- {
-   Serial.println("Vous êtes sur le module : " + strIdModule);
- }
-  
 }
 void loop()
 {
